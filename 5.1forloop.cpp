3de4bfe978e0c412b9079f578268b86723234617
zilihangjian <457284215@qq.com>
2020-04-25 17:12:23 +0800

    for

---
+++ 5.1forloop.cpp
@@ -0,0 +1,11 @@
+// forloop.cpp -- introducing the for loop
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int i;	// create a conter
+	//initialize; test; update
+	for (i = 0; i < 5; i++)
+		cout << "C++ knows when to stop.\n";
+	return 0;
+}
